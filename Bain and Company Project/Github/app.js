2683ccd9e1c01cc832c930b47259fc9dd091f72a
Ervens Achille <55925419+ErvensAchille@users.noreply.github.com>
2021-12-17 21:38:59 -0500

    Update app.js

--- Bain and Company Project/Github/app.js
+++ Bain and Company Project/Github/app.js
@@ -1,4 +1,13 @@
- const convertJSON = json => {
+// convert json into an object
+// get every key from objects > pass in objects > takes every key and put into an array
+// loop through array of keys
+// inside of the function check the type of the key because I want to know what the data looks like
+// switch statement to determine what I'm going to do with those data types...number string and boolean are my most obvious assumptions
+// change everything into some sort of html structure based off the results of the switch statements and push into empty html array 
+// append html to document.body
+
+
+const convertJSON = json => {
    const pojo = JSON.parse(json)
 
    const keys = Object.keys(pojo)
